0d4639236fd5a94eaa8c2a1138e13e28814ab736
Anton Gorbachev <a.gorbachev@bergmann-infotech.de>
2022-05-12 09:51:35 +0300

    123

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import {logger, requestTime} from './middlewares.js';
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT ?? 3000;
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'ejs'));
+
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(requestTime);
+app.use(logger);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
+// });
+
+// app.get('/download', (req, res) => {
+//   console.log(req.requestTime);
+//   res.download(path.resolve(__dirname, 'static', 'index.html'));
+// });
+
+app.listen(PORT, () => {
+  console.log(`Server has been started on port ${PORT}...`);
+});
+
