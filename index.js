2b7af7b3d0b778a9d93d17ef18eeff1f59bd4e35
Anton Gorbachev <a.gorbachev@bergmann-infotech.de>
2022-05-12 12:25:15 +0300

    123

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import {logger, requestTime} from './middlewares.js';
+import serverRoutes from './routes/servers.js';
 
 const __dirname = path.resolve();
 const PORT = process.env.PORT ?? 3000;
@@ -10,8 +11,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, 'ejs'));
 
 app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(requestTime);
 app.use(logger);
+
+app.use(serverRoutes);
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.resolve(__dirname, 'static', 'index.html'));
@@ -21,6 +26,14 @@ app.use(logger);
 //   console.log(req.requestTime);
 //   res.download(path.resolve(__dirname, 'static', 'index.html'));
 // });
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Main Page', active: 'main'});
+});
+
+app.get('/features', (req, res) => {
+  res.render('features', {title: 'Features Page', active: 'features'});
+});
 
 app.listen(PORT, () => {
   console.log(`Server has been started on port ${PORT}...`);
---
+++ static/app.js
@@ -0,0 +1,37 @@
+const App = {
+  data() {
+    return {
+      servers: []
+    }
+  },
+  methods: {
+    async createServer() {
+      const data = {
+        name: this.name,
+        status: 'created'
+      };
+      const res = await fetch('/api/server', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      this.name = '';
+      const newServer = await res.json();
+      this.servers.push(newServer);
+    },
+    async remove(id) {
+      await fetch(`/api/server/${id}`, {method: 'DELETE'});
+      console.log(this.servers );
+      this.servers = this.servers.filter(server => server.id !== id);
+      console.log(this.servers );
+    }
+  },
+  async mounted() {
+    const res = await fetch('/api/server');
+    this.servers = await res.json();
+  }
+};
+
+Vue.createApp(App).mount('#app');
